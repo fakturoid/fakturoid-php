edb5842ad0e7c5aa08e0b5450bbb4cf04f3c2f85
Tlapi <jan@mfcc.cz>
2014-11-14 12:23:28 +0100

    Update Fakturoid.php
    Povolení parametru status tak, abychom mohli získat faktury podle stavu.

--- lib/Fakturoid.php
+++ lib/Fakturoid.php
@@ -43,7 +43,7 @@ class Fakturoid {
   /* Invoice */
   
   public function get_invoices($options = NULL) {
-    return $this->get("/invoices.json" . $this->convert_options($options, array('subject_id', 'since', 'page')));
+    return $this->get("/invoices.json" . $this->convert_options($options, array('subject_id', 'since', 'page', 'status')));
   }
   
   public function get_regular_invoices($options = NULL) {
