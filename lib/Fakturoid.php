ff5114ab47cda5829ae17edef27b711c71c7bb8e
Oldrich Vetesnik <oldrich.vetesnik@gmail.com>
2017-12-07 17:07:45 +0100

    Put class and function opening braces on an empty line.

--- lib/Fakturoid.php
+++ lib/Fakturoid.php
@@ -1,6 +1,6 @@
 <?php
 
-class FakturoidException extends Exception { }
+class FakturoidException extends Exception {}
 
 /* Safety */
 
@@ -11,13 +11,15 @@ if (!function_exists('json_decode')) {
     throw new FakturoidException('Fakturoid lib needs the JSON PHP extension.');
 }
 
-class Fakturoid {
+class Fakturoid
+{
     private $slug;
     private $api_key;
     private $email;
     private $user_agent;
 
-    public function __construct($slug, $email, $api_key, $user_agent) {
+    public function __construct($slug, $email, $api_key, $user_agent)
+    {
         $this->slug       = $slug;
         $this->email      = $email;
         $this->api_key    = $api_key;
@@ -26,195 +28,238 @@ class Fakturoid {
 
     /* Account */
 
-    public function get_account() {
+    public function get_account()
+    {
         return $this->get('/account.json');
     }
 
     /* User */
 
-    public function get_user($id) {
+    public function get_user($id)
+    {
         return $this->get("/users/$id.json");
     }
 
-    public function get_users($options = null) {
+    public function get_users($options = null)
+    {
         return $this->get('/users.json'. $this->convert_options($options, array('page')));
     }
 
     /* Invoice */
 
-    public function get_invoices($options = null) {
+    public function get_invoices($options = null)
+    {
         return $this->get("/invoices.json" . $this->convert_options($options, array('subject_id', 'since', 'updated_since', 'page', 'status', 'custom_id')));
     }
 
-    public function get_regular_invoices($options = null) {
+    public function get_regular_invoices($options = null)
+    {
         return $this->get('/invoices/regular.json'. $this->convert_options($options, array('subject_id', 'since', 'updated_since', 'page', 'status', 'custom_id')));
     }
 
-    public function get_proforma_invoices($options = null) {
+    public function get_proforma_invoices($options = null)
+    {
         return $this->get('/invoices/proforma.json'. $this->convert_options($options, array('subject_id', 'since', 'updated_since', 'page', 'status', 'custom_id')));
     }
 
-    public function get_invoice($id) {
+    public function get_invoice($id)
+    {
         return $this->get("/invoices/$id.json");
     }
 
-    public function get_invoice_pdf($id){
+    public function get_invoice_pdf($id)
+    {
         return $this->run("/invoices/$id/download.pdf", 'get', null, false);
     }
 
-    public function search_invoices($options = null) {
+    public function search_invoices($options = null)
+    {
         return $this->get("/invoices/search.json" . $this->convert_options($options, array('query', 'page')));
     }
 
-    public function update_invoice($id, $data) {
+    public function update_invoice($id, $data)
+    {
         return $this->patch("/invoices/$id.json", $data);
     }
 
-    public function fire_invoice($id, $event, $options = array()) {
+    public function fire_invoice($id, $event, $options = array())
+    {
         return $this->post("/invoices/$id/fire.json", array_merge(array('event' => $event), $options));
     }
 
-    public function create_invoice($data) {
+    public function create_invoice($data)
+    {
         return $this->post('/invoices.json', $data);
     }
 
-    public function delete_invoice($id) {
+    public function delete_invoice($id)
+    {
         return $this->delete("/invoices/$id.json");
     }
 
     /* Expense */
 
-    public function get_expenses($options = null) {
+    public function get_expenses($options = null)
+    {
         return $this->get("/expenses.json" . $this->convert_options($options, array('subject_id', 'since', 'updated_since', 'page', 'status')));
     }
 
-    public function get_expense($id) {
+    public function get_expense($id)
+    {
         return $this->get("/expenses/$id.json");
     }
 
-    public function search_expenses($options = null) {
+    public function search_expenses($options = null)
+    {
         return $this->get("/expenses/search.json" . $this->convert_options($options, array('query', 'page')));
     }
 
-    public function update_expense($id, $data) {
+    public function update_expense($id, $data)
+    {
         return $this->patch("/expenses/$id.json", $data);
     }
 
-    public function fire_expense($id, $event, $options = array()) {
+    public function fire_expense($id, $event, $options = array())
+    {
         return $this->post("/expenses/$id/fire.json", array_merge(array('event' => $event), $options));
     }
 
-    public function create_expense($data) {
+    public function create_expense($data)
+    {
         return $this->post('/expenses.json', $data);
     }
 
-    public function delete_expense($id) {
+    public function delete_expense($id)
+    {
         return $this->delete("/expenses/$id.json");
     }
 
     /* Subject */
 
-    public function get_subjects($options = null) {
+    public function get_subjects($options = null)
+    {
         return $this->get('/subjects.json' . $this->convert_options($options, array('since', 'updated_since', 'page', 'custom_id')));
     }
 
-    public function get_subject($id) {
+    public function get_subject($id)
+    {
         return $this->get("/subjects/$id.json");
     }
 
-    public function create_subject($data) {
+    public function create_subject($data)
+    {
         return $this->post('/subjects.json', $data);
     }
 
-    public function update_subject($id, $data) {
+    public function update_subject($id, $data)
+    {
         return $this->patch("/subjects/$id.json", $data);
     }
 
-    public function delete_subject($id) {
+    public function delete_subject($id)
+    {
         return $this->delete("/subjects/$id.json");
     }
 
-    public function search_subjects($options = null) {
+    public function search_subjects($options = null)
+    {
         return $this->get('/subjects/search.json' . $this->convert_options($options, array('query')));
     }
 
     /* Generator */
 
-    public function get_generators($options = null) {
+    public function get_generators($options = null)
+    {
         return $this->get('/generators.json' . $this->convert_options($options, array('subject_id', 'since', 'updated_since', 'page')));
     }
 
-    public function get_template_generators($options = null) {
+    public function get_template_generators($options = null)
+    {
         return $this->get('/generators/template.json' . $this->convert_options($options, array('subject_id', 'since', 'updated_since', 'page')));
     }
 
-    public function get_recurring_generators($options = null) {
+    public function get_recurring_generators($options = null)
+    {
         return $this->get('/generators/recurring.json' . $this->convert_options($options, array('subject_id', 'since', 'updated_since', 'page')));
     }
 
-    public function get_generator($id) {
+    public function get_generator($id)
+    {
         return $this->get("/generators/$id.json");
     }
 
-    public function create_generator($data) {
+    public function create_generator($data)
+    {
         return $this->post('/generators.json', $data);
     }
 
-    public function update_generator($id, $data) {
+    public function update_generator($id, $data)
+    {
         return $this->patch("/generators/$id.json", $data);
     }
 
-    public function delete_generator($id) {
+    public function delete_generator($id)
+    {
         return $this->delete("/generators/$id.json");
     }
 
     /* Message */
 
-    public function create_message($id, $data) {
+    public function create_message($id, $data)
+    {
         return $this->post("/invoices/$id/message.json", $data);
     }
 
     /* Event */
 
-    public function get_events($options = null) {
+    public function get_events($options = null)
+    {
         return $this->get('/events.json' . $this->convert_options($options, array('subject_id', 'since', 'page')));
     }
 
-    public function get_paid_events($options = null) {
+    public function get_paid_events($options = null)
+    {
         return $this->get('/events/paid.json' . $this->convert_options($options, array('subject_id', 'since', 'page')));
     }
 
     /* Todo */
 
-    public function get_todos($options = null) {
+    public function get_todos($options = null)
+    {
         return $this->get('/todos.json' . $this->convert_options($options, array('subject_id', 'since', 'page')));
     }
 
     /* Helper functions */
 
-    private function get($path) {
+    private function get($path)
+    {
         return $this->run($path, 'get');
     }
 
-    private function post($path, $data) {
+    private function post($path, $data)
+    {
         return $this->run($path, 'post', $data);
     }
 
-    private function put($path, $data) {
+    private function put($path, $data)
+    {
         return $this->run($path, 'put', $data);
     }
 
-    private function patch($path, $data) {
+    private function patch($path, $data)
+    {
         return $this->run($path, 'patch', $data);
     }
 
-    private function delete($path) {
+    private function delete($path)
+    {
         return $this->run($path, 'delete');
     }
 
     /* Query building */
 
-    private function convert_options($options, $allowed) {
+    private function convert_options($options, $allowed)
+    {
         $safe_options = array();
         foreach ($allowed as $key) {
             if (isset($options[$key])) {
@@ -231,7 +276,8 @@ class Fakturoid {
     /**
      * Execute HTTP method on path with data
      */
-    private function run($path, $method, $data = null, $json_decode_return = true) {
+    private function run($path, $method, $data = null, $json_decode_return = true)
+    {
         $c = curl_init();
 
         if ($c === false) {
